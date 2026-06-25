054e8751f09552d88475db96fcc7650fba07e949
Shakunthala15997 <160107334+Shakunthala15997@users.noreply.github.com>
2026-06-25 11:03:00 +0530

    Update mavenbuild.groovy

--- vars/mavenbuild.groovy
+++ vars/mavenbuild.groovy
@@ -1,5 +1,5 @@
 def call(branch,url){
-  git branch:"${branch}" url:"${url}"
+  git branch:"${branch}" ,url:"${url}"
   sh 'mvn clean install'
                 sh "docker build -t hiqode01.jfrog.io/shakunthala-docker/shimage:v1-${BUILD_NUMBER} -f Dockerfile ."
                 sh "docker images"
